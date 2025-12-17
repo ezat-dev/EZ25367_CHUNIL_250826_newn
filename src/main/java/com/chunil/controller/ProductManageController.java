e5b2542b8060df0c89afafed83a10f0698bc4181
Minhyeok Choi <ch07124@naver.com>
2025-12-17 17:14:45 +0900

    1217 경도추가2

--- src/main/java/com/chunil/controller/ProductManageController.java
+++ src/main/java/com/chunil/controller/ProductManageController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2557bc14a083cdac6e7c9ce59cea9077a960a1d4bbb91a6df7ec287e2066ee77
-size 71126
+oid sha256:df8e42863d45f60bd33610cb35585ff1d3022c52ccce8c32aac7a0a70e505423
+size 69920
--- src/main/java/com/chunil/domain/LotReport.java
+++ src/main/java/com/chunil/domain/LotReport.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8f6bca91506697f659d031dda649949b14b5f6db4eecbb1d96aa1ad848ed8b1
-size 27823
+oid sha256:0c7ca548ad31e8f29101a13f6c2a76ea24b52d71bccd9b8ee72bfa37e17dea84
+size 30355
